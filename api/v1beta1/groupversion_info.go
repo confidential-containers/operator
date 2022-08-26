f4980bf4430f135154bc294d41027f568c4a4b7e
Pradipta Banerjee <pradipta.banerjee@gmail.com>
2022-08-26 05:09:44 +0000

    Update code and deps to align with sdk v1.23 and k8s api 0.24.2
    K8s api and golang version used should be in sync with the operator-sdk version.
    For operator-sdk 1.23 that is used for this PR, the corresponding golang and k8s api
    versions can be found here - https://github.com/operator-framework/operator-sdk/blob/v1.23.0/go.mod#L37
    
    Signed-off-by: Pradipta Banerjee <pradipta.banerjee@gmail.com>

--- api/v1beta1/groupversion_info.go
+++ api/v1beta1/groupversion_info.go
@@ -15,8 +15,8 @@ limitations under the License.
 */
 
 // Package v1beta1 contains API Schema definitions for the  v1beta1 API group
-//+kubebuilder:object:generate=true
-//+groupName=confidentialcontainers.org
+// +kubebuilder:object:generate=true
+// +groupName=confidentialcontainers.org
 package v1beta1
 
 import (
