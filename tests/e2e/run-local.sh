6f5c86691d559d6e94bc67b714dbe41cebda56fa
Wainer dos Santos Moschetta <wainersm@redhat.com>
2022-09-01 00:14:20 -0300

    ci: introduce an undo mechanism for bare-metal
    When executed on bare-metal machines, most of the changes applied by the
    ci scripts should be reverted so that it will leave the system as close
    as possible to the initial state and allow subsequent executions.
    
    This introduce a 'undo' mechanism which will attempt to revert the
    changes made by the scripts. You should pass '-u' to run-local.sh as
    `run-local.sh -u` to enable such as feature.
    
    Signed-off-by: Wainer dos Santos Moschetta <wainersm@redhat.com>

--- tests/e2e/run-local.sh
+++ tests/e2e/run-local.sh
@@ -10,6 +10,8 @@ set -o pipefail
 
 script_dir="$(dirname "$(readlink -f "$0")")"
 
+undo="false"
+
 usage() {
 	cat <<-EOF
 	$0: run the end-to-end tests on local host.
@@ -21,13 +23,32 @@ usage() {
 }
 
 parse_args() {
-	while getopts "h" opt; do
+	while getopts "hu" opt; do
 		case $opt in
 			h) usage && exit 0;;
+			u) undo="true";;
 			*) usage && exit 1;;
 		esac
 	done
 }
+
+undo_changes() {
+	# TODO: in case the script failed, we should undo only the steps
+	# executed.
+	pushd "$script_dir" >/dev/null
+	sudo -E PATH="$PATH" ./operator.sh uninstall || true
+	sudo -E PATH="$PATH" ./cluster/down.sh || true
+	ansible-playbook -i localhost, -c local --tags undo ansible/main.yml || true
+	popd
+}
+
+on_exit() {
+	if [ "$undo" == "true" ]; then
+		undo_changes
+	fi
+}
+
+trap on_exit EXIT
 
 main() {
 	parse_args $@
@@ -40,7 +61,7 @@ main() {
 
 	pushd "$script_dir" >/dev/null
 	echo "INFO: Bootstrap the local machine"
-	ansible-playbook -i localhost, -c local ansible/main.yml
+	ansible-playbook -i localhost, -c local --tags untagged ansible/main.yml
 
 	echo "INFO: Bring up the test cluster"
 	sudo ./cluster/up.sh
