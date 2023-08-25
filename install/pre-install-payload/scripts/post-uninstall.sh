4e832acc87c5117a285d00576d5d3b7556a3e6a4
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-08-25 15:24:29 +0200

    pre-install-payload: Rename deploy script
    The script had in its name "container-engine-for-cc-", which made sense
    when we were only using it to deploy the container engine needed for cc.
    
    However, we'll be expanding the functionality of the script to also make
    sure the needed snapshotters are deployed at the users will.
    
    With this in mind, let's simply rename the script to something more
    generic like "reqs-deploy.sh".
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- install/pre-install-payload/scripts/post-uninstall.sh
+++ install/pre-install-payload/scripts/post-uninstall.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-/opt/confidential-containers-pre-install-artifacts/scripts/container-engine-for-cc-deploy.sh uninstall
+/opt/confidential-containers-pre-install-artifacts/scripts/reqs-deploy.sh uninstall
