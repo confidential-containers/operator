083f0f5d839f2549175be581875ef664c785ff1b
Fabiano Fidêncio <fabiano.fidencio@intel.com>
2023-12-13 15:19:11 +0100

    controller: Handle snapshotter per handler
    Let's start properly setting a specific snapshotter per runtime handler
    configured for containerd.
    
    This work depends on a work done on the Kata Containers side to better
    support setting snapshotters per runtime handler.
    
    The PR from the Kata Containers side is:
    https://github.com/kata-containers/kata-containers/pull/8655.
    
    Signed-off-by: Fabiano Fidêncio <fabiano.fidencio@intel.com>

--- controllers/ccruntime_controller.go
+++ controllers/ccruntime_controller.go
@@ -629,15 +629,16 @@ func (r *CcRuntimeReconciler) processDaemonset(operation DaemonOperation) *appsv
 	}
 
 	var runtimeClassNames []string
-	var snapshotter = ""
+	var snapshotter_handler_mapping = ""
 	for _, runtimeClass := range r.ccRuntime.Spec.Config.RuntimeClasses {
 		runtimeClassNames = append(runtimeClassNames, runtimeClass.Name)
-		// FIXME: This will need to be changed by the moment the kata-containers
-		//        payload script supports setting one snapshotter per runtime handler.
-		//        For now, for the v0.8.0 release, we're fine assuming that all the
-		//        set snapshotters are going to be the same.
-		if snapshotter == "" && runtimeClass.Snapshotter != "" {
-			snapshotter = runtimeClass.Snapshotter
+
+		if runtimeClass.Snapshotter != "" {
+			if snapshotter_handler_mapping != "" {
+				snapshotter_handler_mapping += runtimeClass.Name + ":" + runtimeClass.Snapshotter
+			} else {
+				snapshotter_handler_mapping += "," + runtimeClass.Name + ":" + runtimeClass.Snapshotter
+			}
 		}
 	}
 	var runtimeClasses = strings.Join(runtimeClassNames, " ")
@@ -673,8 +674,8 @@ func (r *CcRuntimeReconciler) processDaemonset(operation DaemonOperation) *appsv
 			Value: shims,
 		},
 		{
-			Name:  "SNAPSHOTTER",
-			Value: snapshotter,
+			Name:  "SNAPSHOTTER_HANDLER_MAPPING",
+			Value: snapshotter_handler_mapping,
 		},
 	}
 	envVars = append(envVars, r.ccRuntime.Spec.Config.EnvironmentVariables...)
